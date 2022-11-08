49ad3fdddb815cce5c1b40c446a0490095996b3c
Hubsterrr <hubert.luszczyszyn@gmail.com>
2022-11-08 16:52:12 +0000

    code improvement
    list comprehension added

--- Day 25 - Pandas and CSV files/Day project - U.S. States game/main.py
+++ Day 25 - Pandas and CSV files/Day project - U.S. States game/main.py
@@ -24,13 +24,8 @@ while len(answered_states) < len(data.state):
     answer_state = screen.textinput(title=f"{len(answered_states)}/50 Guess the state", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        not_answered_states = all_states
-
-        for state in all_states:
-            if state not in answered_states:
-                not_answered_states.append(state)
-
-        states_at_the_end = pandas.DataFrame(not_answered_states)
+        missing_states = [state for state in all_states if state not in answered_states]
+        states_at_the_end = pandas.DataFrame(missing_states)
         states_at_the_end.to_csv("missing_states.csv")
         break
 
