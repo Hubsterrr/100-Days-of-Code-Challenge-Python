f4c386e8fd2531aae380f46b2a33dfd2ac62bd13
Hubsterrr <hubert.luszczyszyn@gmail.com>
2022-11-13 16:30:17 +0000

    NATO alphabet update
    added exception catcher that when user will put numbers inside input, program will catch that and put an info that it is not allowed

--- Day 26 - List and Dictionary Comprehensions/Day project - NATO Alphabet/main.py
+++ Day 26 - List and Dictionary Comprehensions/Day project - NATO Alphabet/main.py
@@ -1,14 +1,20 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        word_code = [data_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Only letters are valid.")
+        generate_phonetic()
+    else:
+        print(word_code)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-word_code = [data_dict[letter] for letter in user_input]
-print(word_code)
 
+generate_phonetic()
 
 
 
